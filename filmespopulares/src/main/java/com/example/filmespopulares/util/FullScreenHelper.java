54b5d4aacd16955754307edd794cd95505cf58da
Nina Lofrese <nina.lofrese@gmail.com>
2019-11-12 22:56:04 -0300

    Alterações em FilmesPopulares

--- filmespopulares/src/main/java/com/example/filmespopulares/util/FullScreenHelper.java
+++ filmespopulares/src/main/java/com/example/filmespopulares/util/FullScreenHelper.java
@@ -52,8 +52,7 @@ public class FullScreenHelper {
                         | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                         | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                         | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN);
     }
 
     private void showSystemUi(View mDecorView) {
